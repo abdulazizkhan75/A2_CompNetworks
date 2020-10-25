1f3dab9dd5e84d804e1a158e055dbd7c6bb28450
Ru-Two <darjirutu@gmail.com>
2020-10-25 13:47:13 -0400

    Bug fixes on server

--- jobcreator.py
+++ jobcreator.py
@@ -9,8 +9,7 @@ import random
 # with them through protocols, and exchanges data.
 class JobCreatorNode:
     
-    JOB_TYPES = [""]
-
+    JOB_TYPES = ["ICMP Request", "Craft and send IP packet", "Craft and send TCP packet", "Send HTTP GET request"]
 
     # Defines the server node. Elements within node:
     #  node_socket - socket of this node
@@ -35,35 +34,48 @@ class JobCreatorNode:
 
         self.started_server = True
         while self.started_server:
-            print("Waiting for new connection...")
+            print("Server: Waiting for new connection...")
             self.node_socket.listen(1) #starts listening for connection
             
             connection, address = self.node_socket.accept() #connects to client
-            print("Connected to client: ", address)
             self.total_clients += 1
-            print("Total connected clients: ", self.total_clients)
-            
-            new_thread = threading.Thread(target=self.communicate_with_client, args=(connection, address, ))
+            print("Server: Connected to client #{0}: {1}".format(self.total_clients, address))
+            print("Server: Total clients: ", self.total_clients)
+            client_id = int(connection.recv(1024).decode())
+
+            new_thread = threading.Thread(target=self.communicate_with_client, args=(connection, address, client_id, ))
             new_thread.start()
 
     # Creates new thread to run communications between this server and specified client
-    def communicate_with_client(self, connection, address):
-        keep_connection = True
-        while keep_connection:
-            job = self.JOB_TYPES[random.randint(0,self.JOB_TYPES.len()-1)]
-            connection.send(job.encode())
+    def communicate_with_client(self, connection, address, client_id):
+        try:
+            keep_connection = True
+            while keep_connection:
+                job = self.JOB_TYPES[random.randint(0, len(self.JOB_TYPES) - 1)]
+                print("Server: Asking client {0} to do job - {1}".format(client_id, job))
+                connection.sendall(job.encode())
 
-            response = (connection.recv(1024)).decode()
-            if (response == "REJECT"):
-                print("Client {0} reject job {1}".format(address, job))
-            else:
-                print("Client {0} successfully completed job {1}".format(address, job))
+                response = (connection.recv(1024)).decode()
+                if (response == "REJECT"):
+                    print("Server: Client {0} rejected job - {1}".format(client_id, job))
+                    keep_connection = False
+                    continue
+                else:
+                    print("Server: Client {0} successfully completed job - {1}".format(client_id, job))
 
-            connection.send("AGAIN".encode())
-            response = (connection.recv(1024)).decode()
-            if (response == "NO"):
-                keep_connection = False
-        
-        print("Closing connection with client ", address)
-        self.total_clients -= 1
-        connection.close()
+                print("Server: Asking client {0} to perform job again...".format(client_id))
+                connection.sendall("AGAIN".encode())
+                response = (connection.recv(1024)).decode()
+
+                if (response == "NO"):
+                    keep_connection = False
+
+                print("Server: Client {0}'s response to do job again: {1}".format(client_id, response))
+
+            print("Server: Closing connection with client ", client_id)
+            connection.close()
+        except:
+            print("Server: Closing connection with client ", client_id)
+
+    def getAddress(self):
+        return self.server_address
